10212cfee056bc5f85208cc84180f81c35f405de
mostafizur610 <mostafizurrahman273@gmail.com>
2022-09-19 22:36:39 +0600

    rest countries api destructuring

--- js/countries.js
+++ js/countries.js
@@ -4,20 +4,48 @@ const loadCountries = () => {
         .then(data => displayCountries(data))
 }
 
-displayCountries = countries => {
-    console.log(countries);
+const displayCountries = countries => {
+    console.log(countries[0]);
     const countriesHTML = countries.map(country => getCountryHTML(country));
     // console.log(countriesHTML[0]);
     const container = document.getElementById('countries');
     container.innerHTML = countriesHTML.join(' ');
 }
-const getCountryHTML = country => {
+// option 02 orginal
+// const getCountryHTML = country => {
+//     const { name, flags } = country;
+//     return `
+//     <div class="country">
+//     <h2>${country.name.common}</h2>
+//     <img src="${country.flags.png}">
+//     </div>
+//     `
+// }
+
+// option 01
+// const getCountryHTML = country => {
+//     const { name, flags } = country;
+//     return `
+//     <div class="country">
+//     <h2>${name.common}</h2>
+//     <img src="${flags.png}">
+//     </div>
+//     `
+// }
+
+
+// destructuring option 03
+const getCountryHTML = ({ name, flags, area, region }) => {
+    // const { name, flags } = country;
     return `
     <div class="country">
-    <h2>${country.name.common}</h2>
-    <img src="${country.flags.png}">
+    <h2>${name.common}</h2>
+    <p>Area:${area}</p>
+    <p>Region:${region}</p>
+    <img src="${flags.png}">
     </div>
     `
 }
+
 
 loadCountries()
